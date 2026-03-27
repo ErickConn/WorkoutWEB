3c2f5e1b152ccb393c09322f53cb8e7eb07838a4
Erick Ricardo <erickbbp2005@gmail.com>
2026-03-27 16:22:50 +0000

    update `filtro` component css

--- app/src/components/Filtro.js
+++ app/src/components/Filtro.js
@@ -3,15 +3,15 @@ import styles from './filtro.module.css';
 import { useState } from "react";
 
 export default function Filtro(properties){
-    const [isSelected, setIsSelected] = useState(false);
+    const [isSelected, setIsSelected] = useState(null);
     return(
         <div className={styles.filtro}>
-            <h3>FILTRAR POR GRUPO MUSCULAR</h3>
+            <h3>FILTRAR POR {properties.tipo}</h3>
             <div className={styles.filtros}>
             {properties.filtros.map((filtro, index)=>{
                 return(
-                    <div key={index} className={isSelected? `${styles.selecionado} ${styles.tipo}`: `${styles.tipo}`} onClick={()=>{
-                        setIsSelected((prev)=> !prev);
+                    <div key={index} className={isSelected === index? `${styles.selecionado} ${styles.tipo}`: `${styles.tipo}`} onClick={(e)=>{
+                        setIsSelected(index);
                     }}>
                         <a>{filtro}</a>
                     </div>
