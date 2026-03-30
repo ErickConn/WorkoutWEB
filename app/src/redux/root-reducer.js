95f90df707ea1fc21a81d909e3d15036a3111bfa
Daniel Brito de Souza <danielb.desouza1403@gmail.com>
2026-03-30 00:59:53 +0000

    add app/src/redux/Biometria/actionType.js add app/src/redux/Biometria/actions.js add app/src/redux/Biometria/reduce.js modify app/src/pages/home/Home.js

--- app/src/redux/root-reducer.js
+++ app/src/redux/root-reducer.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 import treinoReducer from "./treino/reduce";
 import userReducer from './user/reduce';
 import sessaoReducer from './sessao/reduce';
+import { biometriaReducer} from './Biometria/reduce';
 
-const rootReducer = combineReducers({userReducer, treinoReducer, sessaoReducer});
+const rootReducer = combineReducers({userReducer, treinoReducer, sessaoReducer, biometriaReducer});
 
 export default rootReducer;
