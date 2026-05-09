f63bfb6c8d00a765c6433c77e7b955a75dd6ac4c
ErickConn <erickbbp2005@gmail.com>
2026-05-08 23:12:11 -0300

    fix: selectPlano and selectTreino

--- backend/models/usuario.js
+++ backend/models/usuario.js
@@ -4,7 +4,18 @@ const usuarioSchema = new mongoose.Schema({
     email: String,
     senha: String,
     role: String,
-    imagem: String
+    imagem: String,
+    activePlanId: String,
+    activeDay: String
+});
+
+usuarioSchema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+    }
 });
 
 const Usuario = mongoose.model('Usuario', usuarioSchema);
--- backend/routes/planos.js
+++ backend/routes/planos.js
@@ -8,11 +8,11 @@ routerPlanos.get('/planos', planosControllers.getAllPlanos);
 
 routerPlanos.get('/planos/:id', planosMiddlewares.validatePlanoId, planosControllers.getPlano);
 
-routerPlanos.post('/planos', planosMiddlewares.validateEmptyBody, planosMiddlewares.normalizeUsers, planosMiddlewares.validatePlanoStructure, planosMiddlewares.validatePlanoTitle, planosControllers.createPlano);
+routerPlanos.post('/planos', planosMiddlewares.validateEmptyBody, planosMiddlewares.validatePlanoStructure, planosMiddlewares.validatePlanoTitle, planosControllers.createPlano);
 
-routerPlanos.patch('/planos/:id', planosMiddlewares.validatePlanoId, planosMiddlewares.validateEmptyBody, planosMiddlewares.normalizeUsers, planosMiddlewares.validatePlanoStructure, planosControllers.patchPlano);
+routerPlanos.patch('/planos/:id', planosMiddlewares.validatePlanoId, planosMiddlewares.validateEmptyBody, planosMiddlewares.validatePlanoStructure, planosControllers.patchPlano);
 
-routerPlanos.put('/planos/:id', planosMiddlewares.validatePlanoId, planosMiddlewares.validateEmptyBody, planosMiddlewares.normalizeUsers, planosMiddlewares.validatePlanoStructure, planosMiddlewares.validatePlanoTitle, planosControllers.putPlano);
+routerPlanos.put('/planos/:id', planosMiddlewares.validatePlanoId, planosMiddlewares.validateEmptyBody, planosMiddlewares.validatePlanoStructure, planosMiddlewares.validatePlanoTitle, planosControllers.putPlano);
 
 routerPlanos.delete('/planos/:id', planosMiddlewares.validatePlanoId, planosControllers.deletePlano);
 
